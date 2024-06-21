059dfe02c38d2714c6d0506cfdee23e929096fbd
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2024-06-21 11:16:56 +0800

    aws cicd jenkins changes

--- springboot-docker-app/src/main/java/com/grstech/springboot_docker_app/controller/DemoDockerCotroller.java
+++ springboot-docker-app/src/main/java/com/grstech/springboot_docker_app/controller/DemoDockerCotroller.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoDockerCotroller {
-	
+
 	@GetMapping("/hello/{name}")
 	public String getHome(@PathVariable String name) {
-		return "Hi "+name +" Welcome to Docker...your docker conteriazation has been done...with FDocker! is Ready" ;
+		return "Hi " + name + " Welcome to Docker...your docker conteriazation has been done...with FDocker! is Ready";
+	}
+
+	@GetMapping("/home")
+	public String getPage() {
+		return "Hi Welcome to AWS...your AWS Jenkin CICD Process Completed Successfully...!";
 	}
 }
